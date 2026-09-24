a1f45baaa7c7e14a2ea8ab541c7633104d89af68
star07316588 <44515399+star07316588@users.noreply.github.com>
2026-09-24 10:06:36 +0800

    Create PrintLabelDto.cs

---
+++ PrintLabelDto.cs
@@ -0,0 +1,8 @@
+public class DropdownRequest
+{
+    public string LabelSpecNo { get; set; }
+    public string CarrierType { get; set; }
+    public string BoxingSpecNo { get; set; }
+    public string Brand { get; set; }
+    public string PinCount { get; set; }
+}
